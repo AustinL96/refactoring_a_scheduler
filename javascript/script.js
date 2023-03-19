7d63392abe28609f61e44890dab62e5ead3706ad
Austin Lewis <austinlewis165@gmail.com>
2023-03-19 13:39:37 -0400

    Finished js. Created working time tracking, store data/retrieval, and current date. Added comments for personal review should I need to come back to it.

--- javascript/script.js
+++ javascript/script.js
@@ -1,48 +1,63 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  $('.saveBtn').click(function() {
-    alert("The button was clicked");
-  });
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO ✔️: Add code to display the current date in the header of the page.
-  //***dddd=current day name, MMMM=full month name, DD=days of month(1-31), 
-  //***YYYY=year (4 digit) */
-  // var date = dayjs().format('dddd MMMM DD YYYY');
-  // // console.log(date)
-  // $('#currentDay').text('Current Date: ' + date)
-  // //***h=hour(12-hour clock)
-  // var currentHour = dayjs().format('h')
-  // //***h=hour(12-hour clock)
-  // var currentPeriod = dayjs().format('A')
-  // // console.log(currentHour + currentPeriod);
 
+$(function () {
+  $('.saveBtn').on('click', function () {
+      //Generates key-value pairs
+      //this.parent refers to the overall row
+      var timeOfDay = $(this).parent().attr('id');
+      //this.sibling refers to the text-area. val() needed to prevent "object Object"
+      //from storing in our key-value pairs
+      var valueText = $(this).siblings('.description').val();
+      // Saves our value from above within local storage
+      localStorage.setItem(timeOfDay, valueText);
+  });
+  //Retrieves the stored value from above and places it in <textarea>
+  $('#hour-9 .description').text(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').text(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').text(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').text(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').text(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').text(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').text(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').text(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').text(localStorage.getItem('hour-17'));
+  // var storedValue = localStorage.getItem('hour-9');
+  // console.log(storedValue)
+
+  function timeBlockFunctionality() {
+      //current hour in military time(0-23)
+      var presentTime = dayjs().hour();
+      // var presentTime = 14
+      // console.log(presentTime)
+      
+      //targets each row to reapply past/present/future classes
+      $('.time-block').each(function () {
+          var businessHour = $(this).attr('id').split('hour-')[1];
+          // console.log($(this).attr('id').split('hour-')[1]);
+          // Compares the 9-5 time to the presentTime to apply bg colors
+          if (businessHour < presentTime) {
+            $(this).addClass('past');
+            $(this).removeClass('present');
+            $(this).removeClass('future');
+          }
+          else if (businessHour == presentTime) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+            $(this).removeClass('future');
+          }
+          else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+          };
+      });
+  };
+  //Calls the function to mark the current time and apply the 3 time-classes
+  timeBlockFunctionality();
 });
 
-
-//***If the var is === to the first hour number, is red. < is green, > is grey */
+//***Tracks the current date
+//***dddd=current day name, MMMM=full month name, DD=days of month(1-31), 
+//***YYYY=year (4 digit) */
 var date = dayjs().format('dddd, MMMM DD YYYY');
-  // console.log(date)
-  $('#currentDay').text('Current Date: ' + date)
-  //***h=hour(12-hour clock)
-  var currentHour = dayjs().format('h')
-  //***h=hour(12-hour clock)
-  var currentPeriod = dayjs().format('A')
-  // console.log(currentHour + currentPeriod);
-  
+// // console.log(date)
+$('#currentDay').text('Current Date: ' + date)
